d0284fd1a671cb1bb93529d1616b252a94e590a0
TuckerH <tucker.humiston.nexus@gmail.com>
2023-08-02 11:58:08 -0500

    Update to tests

---
+++ server/test/Route_Tests/User_Test.js
@@ -0,0 +1,21 @@
+//Everything is only boilerplate rn and will need to be replaced with actual tests
+
+var { assert } = require('chai');
+
+
+describe('User Route Tests', function () {
+  describe('Create user test', function () {
+    it('should create a new user and add it to the database', function () {
+
+
+      assert.equal([1, 2, 3].indexOf(4), -1);
+    });
+  });
+
+  
+  describe('#indexOf()', function () {
+    it('should return -1 when the value is not present', function () {
+      assert.equal([1, 2, 3].indexOf(4), -1);
+    });
+  });
+});
