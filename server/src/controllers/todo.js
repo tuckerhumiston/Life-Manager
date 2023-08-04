0b39acdb35b35cd5e7872bad971c837705bd1ccd
TuckerH <tucker.humiston.nexus@gmail.com>
2023-08-04 13:13:07 -0500

    Finished todo routes

--- server/src/controllers/todo.js
+++ server/src/controllers/todo.js
@@ -108,3 +108,71 @@ exports.add_item = async (req, res) => {
         res.status(500).json({ error: "An error occurred while creating the todo_item" });
     }
 }
+
+//Get all of a user's todo items
+exports.get_all_items = async (req, res) => {
+    try {
+        const { id } = req.user;
+        //Get all todo_items
+        const allTodoItems = await db.query(
+            "SELECT * FROM todo_items WHERE user_id = $1",
+            [id]
+        );
+
+        res.json(allTodoItems.rows);
+    } catch (err) {
+        console.error(err.message);
+    }
+}
+
+//Get all the todo items for a specific todo list
+exports.get_list_items = async (req, res) => {
+    try {
+        const { id } = req.body;
+        const user_id = req.user.id;
+        //Get todo_items
+        const todoItems = await db.query(
+            "SELECT * FROM todo_items WHERE todo_list_id = $1 AND user_id = $2",
+            [id, user_id]
+        );
+
+        res.json(todoItems.rows);
+    } catch (err) {
+        console.error(err.message);
+    }
+}
+
+//Update a todo item
+exports.update_item = async (req, res) => {
+    try {
+        const { id } = req.body;
+        const { title } = req.body;
+        const user_id = req.user.id;
+        //Update todo_item
+        const updatedTodoItem = await db.query(
+            "UPDATE todo_items SET todo = $1 WHERE id = $2 AND user_id = $3 RETURNING *",
+            [title, id, user_id]
+        );
+
+        res.json(updatedTodoItem.rows[0]);
+    } catch (err) {
+        console.error(err.message);
+    }
+}
+
+//Delete a todo item
+exports.delete_item = async (req, res) => {
+    try {
+        const { id } = req.body;
+        const user_id = req.user.id;
+        //Delete todo_item
+        const deletedTodoItem = await db.query(
+            "DELETE FROM todo_items WHERE id = $1 AND user_id = $2 RETURNING *",
+            [id, user_id]
+        );
+
+        res.json(deletedTodoItem.rows[0]);
+    } catch (err) {
+        console.error(err.message);
+    }
+}
--- server/src/routes/auth.js
+++ server/src/routes/auth.js
@@ -3,7 +3,7 @@ const router = Router();
 
 const { register, login, logout, deleteUser} = require('../controllers/auth');
 const { add_list, get_lists, get_list, update_list, delete_list,
-        add_item,  } = require('../controllers/todo');
+        add_item, get_all_items, get_list_items, update_item, delete_item} = require('../controllers/todo');
 
 const { registerValidation, loginValidation } = require('../validators/auth');
 
@@ -11,20 +11,28 @@ const { validationMiddleware } = require('../middleware/validation-middleware');
 const { userAuth } = require('../middleware/passport-middleware');
 
 //Auth Routes
-router.post('/register', registerValidation, validationMiddleware, register);
-router.post('/login', loginValidation, validationMiddleware, login);
-router.get('/logout', userAuth, logout);
-router.delete('/delete-user', userAuth, deleteUser, logout);
+router
+    .post('/register', registerValidation, validationMiddleware, register)
+    .post('/login', loginValidation, validationMiddleware, login)
+    .get('/logout', userAuth, logout)
+    .delete('/delete-user', userAuth, deleteUser, logout);
 
 //Todo List Routes
-router.get('/lists', userAuth, get_lists);
-router.get('/list', userAuth, get_list);
-router.post('/list', userAuth, add_list);
-router.put('/list', userAuth, update_list);
-router.delete('/list', userAuth, delete_list);
+router
+    .get('/lists', userAuth, get_lists)
+    .get('/list', userAuth, get_list)
+    .post('/list', userAuth, add_list)
+    .put('/list', userAuth, update_list)
+    .delete('/list', userAuth, delete_list);
 
 //Todo Item Routes
-router.post('/item', userAuth, add_item);
+router
+    .post('/item', userAuth, add_item)
+    .get('/items', userAuth, get_all_items)
+    .get('/list-items', userAuth, get_list_items)
+    .put('/item', userAuth, update_item)
+    .delete('/item', userAuth, delete_item)
+
 
 
 module.exports = router;
