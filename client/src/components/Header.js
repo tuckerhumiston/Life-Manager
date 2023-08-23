88f49ed8919397e8d83ea3060d0fea1460fc7f83
TuckerH <tucker.humiston.nexus@gmail.com>
2023-08-23 15:16:50 -0500

    Changed logout feature

--- client/src/components/Header.js
+++ client/src/components/Header.js
@@ -1,15 +1,52 @@
-import React from 'react';
+import React, { useState } from 'react';
+import { useDispatch } from 'react-redux';
+
+import { onLogout } from '../api/auth';
+import { unauthenticateUser } from '../redux/slices/authSlice';
+
+
 import '../style//structure/Header.css';
 
 export const Header = () => {
+    const [isDropdownVisible, setDropdownVisible] = useState(false);
+
+    const dispatch = useDispatch();
+
+
+    const toggleDropdown = () => {
+        setDropdownVisible(!isDropdownVisible);
+    };
+
+    // User logout function
+    const logout = async () => {
+        try {
+            await onLogout();
+            dispatch(unauthenticateUser());
+            localStorage.removeItem('isAuth'); //Remove user session from local storage
+        } catch (error) {
+            console.error(error.response);
+        }
+    };
+
             
-            return (
-                <div className="header">
-                    <a href="/">
-                        <img src={require("../style/images/temp-logo.png")} alt="logo"></img>
-                    </a>
-                    <h1>Life Manager</h1>
-                    <img src={require("../style/images/profile-icon.png")} alt="logo"></img>
-                </div>
-            );
+    return (
+        <div className="header">
+            <a href="/">
+                <img src={require("../style/images/temp-logo.png")} alt="logo" />
+            </a>
+            <h1>Life Manager</h1>
+            <div className="profile-container">
+                <img
+                    src={require("../style/images/profile-icon.png")}
+                    alt="profile"
+                    onClick={toggleDropdown}
+                />
+                {isDropdownVisible && (
+                    <ul className="dropdown">
+                        <li onClick={logout}>Logout</li>
+                    </ul>
+                )}
+            </div>
+        </div>
+    );
 };
--- client/src/pages/Dashboard.js
+++ client/src/pages/Dashboard.js
@@ -1,27 +1,12 @@
 import React from 'react';
-import { useDispatch } from 'react-redux';
 
 import { List } from '../components/List';
 import { Quote } from '../components/Quote';
-import { unauthenticateUser } from '../redux/slices/authSlice';
-import { onLogout } from '../api/auth';
 import '../style/Dashboard.css';
 
 
 export const Dashboard = () => {
-    const dispatch = useDispatch();
-
-    // User logout function
-    const logout = async () => {
-        try {
-            await onLogout();
-            dispatch(unauthenticateUser());
-            localStorage.removeItem('isAuth'); //Remove user session from local storage
-        } catch (error) {
-            console.error(error.response);
-        }
-    };
-
+    
     return (
         <div className="dashboard">
             <div className="todo feature">
@@ -53,10 +38,7 @@ export const Dashboard = () => {
             </div>
 
             <div className="extra feature">
-                <h3 className="extra">Controls:</h3>
-                <button className="logout" onClick={logout}>
-                    Logout
-                </button>
+                <h4> More features coming soon!</h4>
             </div>
         </div>
     );
