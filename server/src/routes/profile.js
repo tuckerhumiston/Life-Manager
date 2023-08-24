43a7936a3c26b98af3e9194d53d9ff754075bd64
TuckerH <tucker.humiston.nexus@gmail.com>
2023-08-24 10:53:50 -0500

    Added login rate limiter

--- server/src/routes/profile.js
+++ server/src/routes/profile.js
@@ -6,11 +6,18 @@ const { registerValidation, loginValidation } = require('../validators/profile')
 const { validationMiddleware } = require('../middleware/validation-middleware');
 const { userAuth } = require('../middleware/passport-middleware');
 
+const rateLimit = require("express-rate-limit");
+
+const loginRateLimiter = rateLimit({
+    windowMs: 60 * 60 * 1000, // 1 hour
+    max: 10, // Max 5 login attempts per hour
+    message: "Too many login attempts, please try again later.",
+  });
 
 //Auth Routes
 router
     .post('/register', registerValidation, validationMiddleware, register)
-    .post('/login', loginValidation, validationMiddleware, login)
+    .post('/login', loginValidation, loginRateLimiter, validationMiddleware, login)
     .get('/logout', userAuth, logout)
     .delete('/delete-user', userAuth, deleteUser, logout);
 
